ba083da21874d1cc0c0605fbc298ee84d429d475
Kuttiraja <kuttiraja@gmail.com>
2018-12-08 12:40:58 -0600

    kuttiraja@gmail.com (#5)

--- app/server.js
+++ app/server.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 //Global Middleware
 const bodyParser = require("body-parser")
+const logger = require('./core/logger').logger
 
-//Application Middleware
+//Application Routes imports
 const healthRouter = require('./routes/health')
 
+logger.info('Logging enabled')
+app.use(morgan('combined', {stream: logger.stream}))
+
+//Application Routes
 app.use(healthRouter)
 
-module.exports = app
+module.exports = { 
+    app: app ,  
+    logger: logger 
+}
