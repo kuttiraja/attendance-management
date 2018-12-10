4abe02ec861a07815ae7ed7c941c1eaad11b518e
Kuttiraja <kuttiraja@gmail.com>
2018-12-10 15:20:08 -0600

    Dynamic Route Loading (#10)

--- app/server.js
+++ app/server.js
@@ -2,24 +2,26 @@ const express = require('express')
 const config = require('./core/config')
 const app = express()
 const morgan = require('morgan')
-
+const fs = require('fs')
+const path = require('path')
 
 //Global Middleware
 const bodyParser = require("body-parser")
 const logger = require('./core/logger').logger
 
-//Application Routes imports
-const healthRouter = require('./routes/health')
-const student = require('./routes/student')
+//Dynamic Route Imports & Adding to Express Middleware
+const getDirectories = source => fs.readdirSync(source)
+const routeDir = getDirectories(path.resolve('./app/routes'))
+
+routeDir.map(route => {
+        const router = require(`./routes/${route}`)
+        app.use(`/${config.APP_NAME}/${route}`, router)
+})
 
 logger.info(`Logging enabled ${config.LOG_TO_FILE_OR_CONSOLE}`)
 app.use(morgan('combined', { stream: logger.stream }))
 
-//Application Routes
-app.use('/heartbeat', healthRouter)
-app.use('/student', student)
-
-module.exports = {
-    server: app,
-    logger: logger
+module.exports = { 
+    server: app ,  
+    logger: logger 
 }
