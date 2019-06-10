b175fa665616b90ff93992401e68550ce31e8f05
John Komban <johnpk007@gmail.com>
2019-06-10 09:51:44 -0500

    mark student for removal

--- app/routes/student/index.js
+++ app/routes/student/index.js
@@ -1,4 +1,4 @@
-const { getAllStudent, getStudentById, addStudent, updateStudentById } = require('./student')
+const { getAllStudent, getStudentById, addStudent, updateStudentById, removeStudentById } = require('./student')
 const router = require('express').Router()
 const { studentIDSchema, modifyStudentSchema } = require('./student-schema')
 const { bodyValidator, paramValidator, queryValidator } = require('../../middleware')
@@ -10,5 +10,7 @@ router.get('/:studentId', paramValidator(studentIDSchema), getStudentById)
 router.post('/', addStudent)
 
 router.put('/:studentId', paramValidator(studentIDSchema), bodyValidator(modifyStudentSchema), updateStudentById)
+
+router.put('/:studentId', paramValidator(studentIDSchema), bodyValidator(modifyStudentSchema), removeStudentById)
 
 module.exports = router
--- app/routes/student/student.js
+++ app/routes/student/student.js
@@ -101,10 +101,63 @@ async function updateStudentById(req, res, next) {
         res.status(200).send(response)
 }
 
+async function removeStudentById(req, res, next) {
+    let response = {
+        data: []
+    }
+
+    const { studentId } = req.params
+    logger.info(`student.removeStudentById() - start studentId[${studentId}]`)
+
+    try {
+        const studentsList = await student.getStudentById(studentId)
+        logger.info(`student.removeStudentById() - student.getStudentById() responded [${JSON.stringify(studentsList)}]`)
+
+        if (!studentsList) {
+            response.error = `Error in processing operation`
+        } else if (studentsList && studentsList.length == 0) {
+            response.error = `No student found for ID [${studentId}]`
+        } else if (studentsList && studentsList.length > 1) {
+            response.data.push(studentsList)
+            response.error = `More than 1 stundent found for ID [${studentId}]`
+        } else {
+
+            const { name, dob, emailID, gender, address, parent } = req.body
+            const updStudent = {
+                name,
+                dob,
+                emailID,
+                gender,
+                address,
+                parent,
+                deleted: true
+            }
+
+            logger.info(`student.removeStudentById() - updating student[${studentId}] with [${updStudent}]`)
+            const updResult = await student.updateStudentById(studentId, updStudent)
+            logger.info(`student.removeStudentById() - update sucessfull for student[${studentId}] with [${JSON.stringify(updStudent)}]`)
+            response.data.push(updResult)
+        }
+
+    } catch (err) {
+        response.error = `System failed to find the student with ID [${studentId}]`
+        logger.error(`student.removeStudentById() - error studentId[${studentId}] ${err}`)
+        logger.error(err)
+    }
+
+    logger.info(`student.removeStudentById() - end studentId[${studentId}] result:[${JSON.stringify(response)}]`)
+    if (response.error && response.error.length > 1)
+        res.status(409).send(response)
+    else
+        res.status(200).send(response)
+
+}
+
 
 module.exports = {
     getAllStudent,
     addStudent,
     getStudentById,
-    updateStudentById
+    updateStudentById,
+    removeStudentById
 }
