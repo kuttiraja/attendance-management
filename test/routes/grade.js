53d8820a8fd584e1ce4f50a1b79c5a783da0c71c
John Komban <johnpk007@gmail.com>
2019-06-26 08:26:47 -0500

    updated grade test

--- test/routes/grade.js
+++ test/routes/grade.js
@@ -1,42 +1,42 @@
-const {server} = require('../../app/server')
+const { server } = require('../../app/server')
 const request = require('supertest')
 const chai = require('chai')
 const expect = chai.expect
 const sinon = require('sinon')
 require('sinon-mongoose')
 
-const {gradeModel , counterModel} = require('../../app/db/models')
+const { gradeModel, counterModel } = require('../../app/db/models')
 //mock
 let gradeModelMock, counterModelMock;
 
 //mock-data
-const  gradeData  = require('../data/grade-data')
+const gradeData = require('../data/grade-data')
 const req = request(server)
 
-let  sandbox
-before(()=>{
-    sandbox = sinon.createSandbox()
+let sandbox
+before(() => {
+    sandbox = sinon.createSandbox()
     gradeModelMock = sinon.mock(gradeModel)
-    counterModelMock =sinon.mock(counterModel)
+    counterModelMock = sinon.mock(counterModel)
 
-    
+
     counterModelMock
         .expects('getNextSeqValue').withArgs("gradeId")
         .resolves(1)
 
-var expectedResults = gradeData.gradeDatabyId
-     gradeModelMock
-            .expects('find')   
-            .resolves(expectedResults)
+    var expectedResults = gradeData.gradeDatabyId
+    gradeModelMock
+        .expects('find')
+        .resolves(expectedResults)
 
-var expectedResultall = gradeData.availableGrades
+    var expectedResultall = gradeData.availableGrades
     gradeModelMock
         .expects('find')
         .chain('skip')
         .chain('limit')
         .resolves(expectedResultall)
 
-var expectedResultAdd = gradeData.newGradeDB
+    var expectedResultAdd = gradeData.newGradeDB
     gradeModelMock
         .expects('create')
         .resolves(expectedResultAdd)
@@ -48,36 +48,36 @@ after(() => {
 
 })
 
-describe('grade Testing', () => {
+describe('Grade testing', () => {
     it('Get one grade details', (done) => {
         req
-        .get('/attendancemgmt/grade/1')
-        .end((req, res) => {
-            expect(res.statusCode).to.equal(200)
-            expect(res.body.gradeId).to.equal(1)
-            done()
-        })
+            .get('/attendancemgmt/grade/1')
+            .end((_, res) => {
+                expect(res.statusCode).to.equal(200)
+                expect(res.body.gradeId).to.equal(1)
+                done()
+            })
     })
 
-    it('Get all grade details', (done) => {   
+    it('Get all grade details', (done) => {
         req
-        .get('/attendancemgmt/grade')
-        .query({page_size : 2})
-        .end((req, res) => {
-            expect(res.statusCode).to.equal(200)
-            expect(res.body.length).to.equal(2)
-            done()
-        })
+            .get('/attendancemgmt/grade')
+            .query({ page_size: 2 })
+            .end((_, res) => {
+                expect(res.statusCode).to.equal(200)
+                expect(res.body.length).to.equal(2)
+                done()
+            })
     })
 
-    it('Add one grade', (done) => {   
+    it('Add one grade', (done) => {
         req
-        .post('/attendancemgmt/grade')
-        .send(gradeData.newGrade)
-        .end((req, res) => {
-            expect(res.statusCode).to.equal(201)
-            expect(res.body.gradeId).to.equal(1)
-            done()
-        })
+            .post('/attendancemgmt/grade')
+            .send(gradeData.newGrade)
+            .end((_, res) => {
+                expect(res.statusCode).to.equal(201)
+                expect(res.body.gradeId).to.equal(1)
+                done()
+            })
     })
 })
